d8e6a1a088741a5e12927c638d06293c99de42a3
Kyle Conley <kyle.conley@me.com>
2017-11-05 12:46:57 -0800

    moving to github

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+
+//  requires that floor, ceiling, and start be passed in as query params example
+//  http://localhost:8080/elevator?floor=0&ceiling=19&start=10
+app.get('/elevator', (req, res) => {
+    let floor = parseInt(req.query.floor);
+    let ceiling = parseInt(req.query.ceiling);
+    let start = parseInt(req.query.start);
+
+    //error checking
+    if ((!floor && floor !== 0) || (!ceiling && ceiling !== 0) || (!start && start !== 0)) {
+        res.status(400).json({
+            "error": "Oops! Looks like you forgot the floor, ceiling, and/or start query params"
+        })
+    } else {
+        let result = move(floor, ceiling, start);
+        res.status(200).json(result);
+
+    }
+
+    function move(floor, ceiling, start) {
+
+        let directions = ['u1', 'd5', 'u2', 'u9', 'd14'];
+        let distance = 0;
+        let currentPos = start;
+        for (let i = 0; i < directions.length; i++) {
+            let currentDirection = directions[i];
+            let curentDistance = parseInt(currentDirection.slice(1));
+            if (currentDirection.slice(0, 1) === 'u') { //going up
+                distance += curentDistance; //distance traveled
+                currentPos += curentDistance; //ending floor for each index of directions
+                if (currentPos > ceiling) {
+                    let dif = currentPos - ceiling;
+                    currentPos = ceiling;
+                    distance -= dif;
+                }
+            } else if (currentDirection.slice(0, 1) === 'd') { //going down
+                distance += curentDistance; //distance traveled
+                currentPos -= curentDistance; //ending floor for each index of directions
+                if (currentPos < floor) {
+                    let dif = floor - currentPos;
+                    currentPos = floor;
+                    distance -= dif;
+                }
+            }
+        }
+        let distanceFromStart = currentPos - start;
+        return {
+            "Total Distance Traveled": distance,
+            "End": currentPos,
+            "Distance From Start": distanceFromStart
+        }
+    }
+    
+
+})
+
+
+
+app.listen('8080', () => {
+    console.log('Magic happens on port 8080');
+})
